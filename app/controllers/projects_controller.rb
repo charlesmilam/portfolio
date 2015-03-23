42c6953a57dfcf1a2600175a3626d277e15eabfc
charles milam <charles.milam@gmail.com>
2015-03-23 16:34:15 -0500

    Modify form partial for projects to make use of url field. Modify projects index page to use url and use it in link in action portion of card.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -70,6 +70,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :about, :dev_date, :repo, :status, :image)
+      params.require(:project).permit(:name, :about, :dev_date, :repo, :url, :status, :image)
     end
 end
