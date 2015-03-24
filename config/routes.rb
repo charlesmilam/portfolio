8568a19ccb5f9052126a6d0f8d6022c9fbd48419
charles milam <charles.milam@gmail.com>
2015-03-24 17:56:06 -0500

    Modify routes to to skip registrations for admins.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :projects
 
-  devise_for :admins
+  devise_for :admins, skip: :registrations
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
